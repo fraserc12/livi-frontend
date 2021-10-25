207e1a9694cfc1fcab0144f4f172cbe61ab04e33
fraserc12 <fraser.cassidy@hbr.org>
2021-10-25 10:12:25 +0100

    initial commit: fetch health data and display in table

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM node:lts-alpine
+
+WORKDIR /usr/src/app
+
+COPY package.json package-lock.json ./
+RUN npm install
+COPY . ./
+
+CMD [ "npm", "start" ]
--- src/App.js
+++ src/App.js
@@ -1,24 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { useTable } from 'react-table'
 
 function App() {
+  const [healthData, setHealthData] = useState([]);
+
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: 'Service Name',
+        accessor: 'serviceName',
+      },
+      {
+        Header: 'Service URL',
+        accessor: 'url', 
+      },
+      {
+        Header: 'Status',
+        accessor: 'status',
+      },
+      {
+        Header: 'Updated',
+        accessor: 'creationTime',
+      },
+    ],
+    []
+  )
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = useTable({ columns, data: healthData })
+
+  const getAppHealthStatus = async () => {
+    const res = await fetch("http://localhost:8082/external/all");
+    const data = await res.json();
+    setHealthData(data);
+  };
+
+  useEffect(() => {
+    const timer = setInterval(getAppHealthStatus, 30000);
+    return () => clearInterval(timer);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>Service Health Status</h1>
+    <table {...getTableProps()} style={{ border: 'solid 1px #33ccff' }}>
+       <thead>
+         {headerGroups.map(headerGroup => (
+           <tr {...headerGroup.getHeaderGroupProps()}>
+             {headerGroup.headers.map(column => (
+               <th
+                 {...column.getHeaderProps()}
+                 style={{
+                   borderBottom: 'solid 3px #ff6666',
+                   background: 'aliceblue',
+                   color: 'black',
+                   fontWeight: 'bold',
+                 }}
+               >
+                 {column.render('Header')}
+               </th>
+             ))}
+           </tr>
+         ))}
+       </thead>
+       <tbody {...getTableBodyProps()}>
+         {rows.map(row => {
+           prepareRow(row)
+           return (
+             <tr {...row.getRowProps()}>
+               {row.cells.map(cell => {
+                 return (
+                   <td
+                      onClick={() => console.info(row.values.team, cell.value)}
+                     {...cell.getCellProps()}
+                     style={{
+                       padding: '10px',
+                       border: 'solid 1px gray',
+                       background: cell.value === 'FAIL' ? 'red' : 'papayawhip',
+                     }}
+                   >
+                     {cell.render('Cell')}
+                   </td>
+                 )
+               })}
+             </tr>
+           )
+         })}
+       </tbody>
+     </table>
+     </>
   );
 }
 
